c09a23d558177fce441c10c9c41a82ddc3ebaf96
lahanatou <nouryous30@gmail.com>
2023-02-05 15:16:00 +0100

    new commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -80,22 +80,22 @@ originalparent5 = Hostparent.create!(
         phone: "123456",
             )
 
-hostparent4 = Hostparent.create!(
-        name: "hostparent2",
+hostparent1 = Hostparent.create!(
+        name: "hostparent1",
+        description:"adress1",
+        phone: "123456",
+            )
+
+hostparent2 = Hostparent.create!(
+        name: "originalparent2",
         description:"adress2",
         phone: "123456",
             )
 
-hostparent4 = Hostparent.create!(
-        name: "originalparent3",
+
+hostparent3 = Hostparent.create!(
+        name: "hostparent3",
         description:"adress3",
-        phone: "123456",
-            )
-
-
-hostparent5 = Hostparent.create!(
-        name: "hostparent4",
-        description:"adress4",
         phone: "123456",
             )
 
@@ -109,5 +109,51 @@ hostparent4 = Hostparent.create!(
 hostparent5 = Hostparent.create!(
         name: "hostparent5",
         description:"adress5"",
+        phone: "123456",
+        )
+
+
+orphan1 = Orphan.create!(
+        name:"hostparent1",
+        description:"adress1",
+        age:"age1",
+        image:"image1",
+        sex:"sex1",
+        phone: "123456",
+        )
+
+orphan2 = Orphan.create!(
+        name:"hostparent2",
+        description:"adress2",
+        age:"age2",
+        image:"image2",
+        sex:"sex2",
+        phone: "123456",
+        )
+
+orphan3 = Orphan.create!(
+        name: "hostparent3",
+        description:"adress3",
+        age:"age3",
+        image:"image3",
+        sex:"sex3",
+        phone: "123456",
+        )
+
+orphan4 = Orphan.create!(
+        name: "hostparent4",
+        description:"adress4",
+        age:"age4",
+        image:"image4",
+        sex:"sex4",
+        phone: "123456",
+        )
+
+orphan5 = Orphan.create!(
+        name: "hostparent5",
+        description:"adress5",
+        age:"age5",
+        image:"image5",
+        sex:"sex5",
         phone: "123456",
         )
