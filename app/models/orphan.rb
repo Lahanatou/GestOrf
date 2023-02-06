22456562bcf986884e19224902fa993a3adb75bc
lahanatou <nouryous30@gmail.com>
2023-02-07 00:04:54 +0100

    new commit

--- app/models/orphan.rb
+++ app/models/orphan.rb
@@ -1,11 +1,12 @@
 class Orphan < ApplicationRecord
   belongs_to :hostparent
   belongs_to :originalparent
+  #validates :name,:description,:sexe,:age, null:false, presence: true, unless: :image?
   #belongs_to :user
-validates :name, :description, null:false, presence: true, length: { minimum: 1 }, unless: :image?
-validates :age, presence: false
-validates :sex, presence: false
-validates :image, presence: false
+validates :name, :description, :sexe, :age ,null:false, presence: true, length: { minimum: 1 }, unless: :image?
+# validates :age, presence: false
+# validates :sex, presence: false
+# validates :image, presence: false
 mount_uploader :image, ImageUploader
 #paginates_per 10
 end
