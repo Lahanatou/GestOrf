b259867ec712a46dfa12efe4ff9aaf2c7bda3430
lahanatou <nouryous30@gmail.com>
2023-02-12 14:58:48 +0100

    new commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  #before_action :check_admin, only: [:edit, :create, :update, :destroy]
 
     protected
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     end
+  #   def check_admin
+  #   unless current_user.admin
+  #       redirect_to '/', :alert => "Don't have permission!"
+  #   end
+  # end
   end
